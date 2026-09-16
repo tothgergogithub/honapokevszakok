4b43616593686e8701d3ae67559ccb59e5d2613a
tothgergogithub <tothgergo@turr.hu>
2026-09-16 11:34:59 +0200

    honapok

---
+++ main.js
@@ -0,0 +1,69 @@
+function bekeres(){
+
+let beker=prompt("Adj meg egy hónapszámot")
+}
+let honapok=[
+
+    januar={
+    honap: "Január",
+    evszak: "Tél",
+    nap: 31
+},
+    februar={
+    honap: "Február",
+    evszak: "Tél",
+    nap:28 
+    },
+
+    marcius={
+    honap: "Március",
+    evszak: "Tavasz",
+    nap: 31
+    },
+    aprilis={
+    honap: "Április",
+    evszak: "Tavasz",
+    nap: 30
+    },
+    majus={
+    honap: "Május",
+    evszak: "Tavasz",
+    nap:31
+    },
+    junius={
+    honap:"Június",
+    evszak: "Nyár",
+    nap:30
+    },
+    julius={
+    honap:"Július",
+    evszak: "Nyár",
+    nap:31
+    },
+    augusztus={
+    honap:"Augusztus",
+    evszak: "Nyár",
+    nap:31
+    },
+    szeptember={
+    honap:"Szeptember",
+    evszak: "Ősz",
+    nap:30
+    },
+    oktober={
+    honap:"Október",
+    evszak: "Nyár",
+    nap:30
+    },
+    november={
+        honap:"November",
+        evszak:"Ősz",
+        nap:30
+    },
+    december={
+        honap:"December",
+        evszak:"Tél",
+        nap: 31
+    }
+]
+
