62461757dd2d4a3e49a0208654019a04746f6344
tothgergogithub <tothgergo@turr.hu>
2026-09-16 12:30:03 +0200

    hibakezelés

--- main.js
+++ main.js
@@ -1,58 +1,57 @@
-function bekeres(){
 
-let beker=prompt("Adj meg egy hónapszámot")
-}
+
+
 let honapok=[
 
-    januar={
+    {
     honap: "Január",
     evszak: "Tél",
     nap: 31
 },
-    februar={
+    {
     honap: "Február",
     evszak: "Tél",
     nap:28 
     },
 
-    marcius={
+    {
     honap: "Március",
     evszak: "Tavasz",
     nap: 31
     },
-    aprilis={
+    {
     honap: "Április",
     evszak: "Tavasz",
     nap: 30
     },
-    majus={
+    {
     honap: "Május",
     evszak: "Tavasz",
     nap:31
     },
-    junius={
+    {
     honap:"Június",
     evszak: "Nyár",
     nap:30
     },
-    julius={
+    {
     honap:"Július",
     evszak: "Nyár",
     nap:31
     },
-    augusztus={
+    {
     honap:"Augusztus",
     evszak: "Nyár",
     nap:31
     },
-    szeptember={
+    {
     honap:"Szeptember",
     evszak: "Ősz",
     nap:30
     },
-    oktober={
+    {
     honap:"Október",
-    evszak: "Nyár",
+    evszak: "Ősz",
     nap:30
     },
     november={
@@ -67,3 +66,28 @@ let honapok=[
     }
 ]
 
+
+
+function honapbekeres(honapszam){
+    let elem=honapok[honapszam-1];
+    console.log(elem)
+    return elem
+    
+}
+try{
+    const beker=prompt("írj be egy számot")
+    if(honapszam<1|| honapszam>12){
+        throw new Error("Hibás számot adtál meg")
+    }
+    if(isNaN(honapszam)){
+        throw new Error("Nem számot adtál meg")
+    }
+    else{
+        const honap=honapbekeres(honapszam);
+        console.log("Hónap", honap.honap)
+    }
+
+}
+catch(e){
+    console.log("Hiba")
+}
